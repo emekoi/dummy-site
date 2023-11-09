b845ca1f560c109a7b9d128a663396e27d5e780f
Emeka Nkurumeh <emekankurumeh@outlook.com>
2023-11-09 04:55:15 -0800

    initial commit

---
+++ deploy-page.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh -e
+
+head="$(git rev-parse --abbrev-ref HEAD)"
+
+site_branch="${1:-gh-pages}"
+build_dir="${2:-build}"
+
+setup_branch() {
+  git switch --orphan "$site_branch"
+  git commit --allow-empty -m"init"
+  git switch "$head"
+}
+
+build_page() {
+  mkdir -p "$build_dir"
+  ./build.sh > "$build_dir"/index.html
+  git -C "$build_dir" add --all
+  git -C "$build_dir" commit -m "deploy from $(git rev-parse "$head")"
+}
+
+build_page_clean() {
+  [ -d "$build_dir" ] && [ -f "$build_dir"/.git ] && git worktree remove "$build_dir"
+  [ -d "$build_dir" ] && rm -r "$build_dir"
+  git worktree add "$build_dir" "$site_branch"
+  build_page
+  git worktree remove "$build_dir"
+}
+
+build_page_dirty() {
+  [ -d "$build_dir" ] && [ ! -f "$build_dir"/.git ] && rm -r "$build_dir"
+  [ ! -d "$build_dir" ] && git worktree add "$build_dir" "$site_branch"
+  build_page
+}
+
+git rev-parse --verify "$site_branch" >/dev/null 2>&1 || setup_branch
+([ -z "$DIRTY" ] && build_page_clean) || build_page_dirty
